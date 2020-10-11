389566d93f07f022fc7e992f107b2dcf84f062f3
Radomir Radionov <66359081+radomir-radionov@users.noreply.github.com>
2020-10-11 09:25:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,178 @@
+
+// Задание 1
+
+// let result;
+// let arr = [1, 2, 3];
+// if (arr.length === 3) {
+//   console.log(arr[0] + arr[1] + arr[2]);
+// }
+
+// Задание 2
+
+// let num = 1030;
+// let str = String(num);
+
+// if (str[str.length - 1] === "0") {
+//   console.log("верно");
+// } else {
+//   console.log("неверно");
+// }
+
+// Задание 3 not ready
+
+// let num = 31;
+// let str = String(num);
+// if (str[str.length - 1] === "0") {
+//   console.log("верно");
+// }
+// else if (str[str.length - 1] === "2") {
+//   console.log("верно");
+// }
+// else if (str[str.length - 1] === "4") {
+//   console.log("верно");
+// }
+// else if (str[str.length - 1] === "6") {
+//   console.log("верно");
+// }
+// else if (str[str.length - 1] === "8") {
+//   console.log("верно");
+// } else {
+//   console.log('error')
+// }
+
+// Задание 4
+
+// let a = 9;
+// let b = 2;
+// let rest = a % b;
+
+// if (rest == 0) {
+//   console.log("Четное");
+// } else {
+//   console.log("Нечётное " + rest);
+// }
+
+// Задание 5
+
+// let a = 38;
+// let b = 3;
+// let rest = a % b;
+
+// if (rest == 0) {
+//   console.log("Делится на 3.");
+// } else {
+//   console.log("Не делится на 3." + `Остаток: ${rest}`);
+// }
+
+// Задание 6
+
+// let month = 7;
+// if (month <= 3) {
+//   console.log("Зима");   
+// }
+// if (month <= 6 && month > 3) {
+//   console.log("Весна");
+// }
+// if (month <= 9 && month > 6) {
+//   console.log("Лето");
+// }
+// if (month <= 12 && month > 9) {
+//   console.log("Осень");
+// }
+
+// Задание 7
+
+// let str = "abcde";
+// if(str[0] === 'a'){
+//   console.log('Да')
+// } else { console.log('Нет')}
+
+// Задание 8
+// let num = 12345;
+// let str = String(num)
+// if (str[0] === '1'){
+//   console.log('Yes')
+// } else if (str[0] === '2'){
+//   console.log('Yes')}
+// else if (str[0] === '3'){
+// console.log('Yes')} 
+// else {
+//   console.log('Error')
+// }
+
+// Задание 9
+
+// let num = 123
+// let str = String(num)
+// console.log(+str[0]+ + str[1] + + str[2])
+// Задание 10
+
+// let num = 123123
+// let str = String(num)
+// let first = +str[0]+ + str[1]+ +str[2]
+// let second = +str[3]+ + str[4]+ +str[5]
+// if (first === second) {console.log(
+//   'Yes'
+// )} else {
+//   'No'
+// }
+
+// Задание 10.5
+
+// let num = 5;
+
+// switch (num) {
+// 	case 1:
+// 		alert('Зима');
+// 	break;
+// 	case 2:
+// 		alert('Весна');
+// 	break;
+// 	case 3:
+// 		alert('Лето');
+// 	break;
+// 	case 4:
+// 		alert('Осень');
+//   break;
+//   default:
+// 		alert('Error');
+// 	break;
+// }
+
+// Задание 11
+
+// let num = -10
+// let result = num >= 0 ? 1: -1;
+// console.log(result)
+
+// Задание 12
+
+// let a = 2 * (3 - 1);
+// let b = 6 - 2;
+// let result = a === b
+// console.log(result)
+
+// Задание 13
+
+// let a = 1 * (7 - 4);
+// let b = 1 + 2 + 7;
+// let result = a > b
+// console.log(result)
+
+// Задание 14
+
+// let a = 2 ** 4;
+// let b = 4 ** 2;
+// let result = a!=b
+// console.log(result)
+
+// Задание 15
+
+// let ok = confirm('Вам есть 18?');
+
+// if (ok) {
+// 	alert('для взрослых');
+// } else {
+// 	alert('доступ пользователю запрещен');
+// };
+
